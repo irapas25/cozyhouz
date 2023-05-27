895b3f523ae48fd6bb635e78166be777b8694b0f
Pascal Irankunda <irapas25@yahoo.fr>
2023-05-26 23:33:40 -0600

    added mongo sanitize

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require('helmet');
-// const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const rentalRoutes = require('./routes/rentals');
 const reviewRoutes = require('./routes/reviews');
@@ -44,9 +44,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(mongoSanitize({
-//   replaceWith: '_'
-// }));
+app.use(mongoSanitize({
+  replaceWith: '_'
+}));
 
 const MongoStore = require('connect-mongo');
 
